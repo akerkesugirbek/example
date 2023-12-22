027f7feaec4ed0b0fa119b05f8ff8a2c64b4682c
akerk <231501@astanait.edu.kz>
2023-12-22 10:03:02 +0600

    java

---
+++ src/Point.java
@@ -0,0 +1,13 @@
+import java.lang.Math;
+public class Point{
+    private double x;
+    private double y;
+
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+    public double distanceTo(Point dest){
+        return Math.sqrt(Math.pow((this.x - dest.x), 2)+Math.pow((this.y - dest.y), 2));
+    }
+}
